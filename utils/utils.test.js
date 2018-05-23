9435dac97b3dad7dbeae63b7be80199b2b866482
Daniel Ciuraru <daniel86mail@gmail.com>
2018-05-23 17:22:16 +0300

    tests part 1

---
+++ utils/utils.test.js
@@ -0,0 +1,10 @@
+const utils = require('./utils');
+
+it('should add two numbers', () => {
+    var res = utils.add(11,33);
+    
+});
+
+it('should square a number', () => {
+    var res = utils.square(3);
+});
