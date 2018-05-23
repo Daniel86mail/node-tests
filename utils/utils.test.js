1947cae7f31e0cf41f9697419e86bc2175454c5f
Daniel Ciuraru <daniel86mail@gmail.com>
2018-05-23 17:49:21 +0300

    testing async functions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,10 +1,34 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     var res = utils.add(11,33);
-    
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var res = utils.square(3);
+    expect(res).toBe(9);
+});
+
+it('should split the full name', () => {
+    var user = {
+        firstName: '',
+        lastName: '',
+        fullName: 'Daniel Ciuraru',
+        age: 31
+    };
+    var res = utils.setName(user, user.fullName);
+    expect(res.firstName).toEqual('Daniel');
+    expect(res.lastName).toEqual('Ciuraru');
+    expect(res).toInclude({
+        age: 31
+    });
+});
+
+it('should add async add two numbers', (done) => {
+    utils.asyncAdd(4,3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    });
 });
